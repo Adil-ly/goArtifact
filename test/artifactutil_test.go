edbd52b43bd73a12c46d03865b3072ad8a5c048d
1172498005@qq.com <1172498005@qq.com>
2023-08-25 17:53:45 +0800

    修改测试用例

--- test/artifactutil_test.go
+++ test/artifactutil_test.go
@@ -11,7 +11,7 @@ import (
 //	@param t
 func TestArtifactUtilIsInSliceWithInt(t *testing.T) {
 	dataSlice := []int{1, 2, 35, 6, 7, 8}
-	dataInt := 7
+	dataInt := 8
 	res := goArtifact.NewArtifactUtil().IsInSliceWithInt(dataInt, dataSlice)
 	t.Logf("结果：%t", res)
 }
