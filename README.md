09483c26a66b5ee4cb1bd5445d98a39b4c106fe9
1172498005@qq.com <1172498005@qq.com>
2023-08-28 14:17:46 +0800

    修改

--- README.md
+++ README.md
@@ -19,9 +19,16 @@ util := goArtifact.NewArtifactUtil()
 // 验证某个元素是否存在int切片中
 util.IsInSliceWithInt() 
 util.IsInSliceWithInt8()    
+util.IsInSliceWithInt16() 
 util.IsInSliceWithInt32()
 util.IsInSliceWithInt64()
-
+-------
+// 验证某个元素是否存在uint切片中
+util.IsInSliceWithUint() 
+util.IsInSliceWithUint8()    
+util.IsInSliceWithUint16() 
+util.IsInSliceWithUint32()
+util.IsInSliceWithUint64()
 -------
 // 验证某个元素是否存在string切片中
 util.IsInSliceWithString()
@@ -30,8 +37,17 @@ util.IsInSliceWithString()
 // int类型的去重
 util.ReDupWithInt() 
 util.ReDupWithInt8()
+util.ReDupWithInt16()
 util.ReDupWithInt32()
 util.ReDupWithInt64()
+
+-------
+// uint类型的去重
+util.ReDupWithUint()
+util.ReDupWithUint8()
+util.ReDupWithUint16()
+util.ReDupWithUint32()
+util.ReDupWithUint64()
 
 -------
 // string类型的去重
--- artifactutil.go
+++ artifactutil.go
@@ -39,6 +39,22 @@ func (a *ArtifactUtil) IsInSliceWithInt8(target int8, targetSlice []int8) bool {
 	return false
 }
 
+// IsInSliceWithInt16
+//
+//	@Description: 判断当前Int16的是否存在于切片中
+//	@receiver a
+//	@param target
+//	@param targetSlice
+//	@return bool
+func (a *ArtifactUtil) IsInSliceWithInt16(target int16, targetSlice []int16) bool {
+	for _, item := range targetSlice {
+		if target == item {
+			return true
+		}
+	}
+	return false
+}
+
 // IsInSliceWithInt32
 //
 //	@Description: 判断当前Int32的是否存在于切片中
@@ -63,6 +79,86 @@ func (a *ArtifactUtil) IsInSliceWithInt32(target int32, targetSlice []int32) boo
 //	@param targetSlice
 //	@return bool
 func (a *ArtifactUtil) IsInSliceWithInt64(target int64, targetSlice []int64) bool {
+	for _, item := range targetSlice {
+		if target == item {
+			return true
+		}
+	}
+	return false
+}
+
+// IsInSliceWithUint
+//
+//	@Description: 判断当前Uint的是否存在于切片中
+//	@receiver a
+//	@param target
+//	@param targetSlice
+//	@return bool
+func (a *ArtifactUtil) IsInSliceWithUint(target uint, targetSlice []uint) bool {
+	for _, item := range targetSlice {
+		if target == item {
+			return true
+		}
+	}
+	return false
+}
+
+// IsInSliceWithUint8
+//
+//	@Description: 判断当前Uint8的是否存在于切片中
+//	@receiver a
+//	@param target
+//	@param targetSlice
+//	@return bool
+func (a *ArtifactUtil) IsInSliceWithUint8(target uint8, targetSlice []uint8) bool {
+	for _, item := range targetSlice {
+		if target == item {
+			return true
+		}
+	}
+	return false
+}
+
+// IsInSliceWithUint16
+//
+//	@Description: 判断当前Uint16的是否存在于切片中
+//	@receiver a
+//	@param target
+//	@param targetSlice
+//	@return bool
+func (a *ArtifactUtil) IsInSliceWithUint16(target uint16, targetSlice []uint16) bool {
+	for _, item := range targetSlice {
+		if target == item {
+			return true
+		}
+	}
+	return false
+}
+
+// IsInSliceWithUint32
+//
+//	@Description: 判断当前Uint32的是否存在于切片中
+//	@receiver a
+//	@param target
+//	@param targetSlice
+//	@return bool
+func (a *ArtifactUtil) IsInSliceWithUint32(target uint32, targetSlice []uint32) bool {
+	for _, item := range targetSlice {
+		if target == item {
+			return true
+		}
+	}
+	return false
+}
+
+// IsInSliceWithUint64
+//
+//	@Description: 判断当前Uint64的是否存在于切片中
+//	@receiver a
+//	@param target
+//	@param targetSlice
+//	@return bool
+func (a *ArtifactUtil) IsInSliceWithUint64(target uint64, targetSlice []uint64) bool {
 	for _, item := range targetSlice {
 		if target == item {
 			return true
@@ -141,6 +237,24 @@ func (a *ArtifactUtil) ReDupWithInt8(params []int8) []int8 {
 	return res
 }
 
+// ReDupWithInt16
+//
+//	@Description: Int16切片的去重
+//	@receiver a
+//	@param params
+//	@return []int16
+func (a *ArtifactUtil) ReDupWithInt16(params []int16) []int16 {
+	res := make([]int16, 0, len(params))
+	tmpMap := map[int16]struct{}{}
+	for _, item := range params {
+		if _, exists := tmpMap[item]; !exists {
+			tmpMap[item] = struct{}{}
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
 // ReDupWithInt32
 //
 //	@Description: Int32切片的去重
@@ -168,6 +282,96 @@ func (a *ArtifactUtil) ReDupWithInt32(params []int32) []int32 {
 func (a *ArtifactUtil) ReDupWithInt64(params []int64) []int64 {
 	res := make([]int64, 0, len(params))
 	tmpMap := map[int64]struct{}{}
+	for _, item := range params {
+		if _, exists := tmpMap[item]; !exists {
+			tmpMap[item] = struct{}{}
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+// ReDupWithUint
+//
+//	@Description: Uint切片的去重
+//	@receiver a
+//	@param params
+//	@return []uint
+func (a *ArtifactUtil) ReDupWithUint(params []uint) []uint {
+	res := make([]uint, 0, len(params))
+	tmpMap := map[uint]struct{}{}
+	for _, item := range params {
+		if _, exists := tmpMap[item]; !exists {
+			tmpMap[item] = struct{}{}
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+// ReDupWithUint8
+//
+//	@Description: Uint8切片的去重
+//	@receiver a
+//	@param params
+//	@return []uint8
+func (a *ArtifactUtil) ReDupWithUint8(params []uint8) []uint8 {
+	res := make([]uint8, 0, len(params))
+	tmpMap := map[uint8]struct{}{}
+	for _, item := range params {
+		if _, exists := tmpMap[item]; !exists {
+			tmpMap[item] = struct{}{}
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+// ReDupWithUint16
+//
+//	@Description: Uint16切片的去重
+//	@receiver a
+//	@param params
+//	@return []uint16
+func (a *ArtifactUtil) ReDupWithUint16(params []uint16) []uint16 {
+	res := make([]uint16, 0, len(params))
+	tmpMap := map[uint16]struct{}{}
+	for _, item := range params {
+		if _, exists := tmpMap[item]; !exists {
+			tmpMap[item] = struct{}{}
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+// ReDupWithUint32
+//
+//	@Description: Uint32切片的去重
+//	@receiver a
+//	@param params
+//	@return []uint32
+func (a *ArtifactUtil) ReDupWithUint32(params []uint32) []uint32 {
+	res := make([]uint32, 0, len(params))
+	tmpMap := map[uint32]struct{}{}
+	for _, item := range params {
+		if _, exists := tmpMap[item]; !exists {
+			tmpMap[item] = struct{}{}
+			res = append(res, item)
+		}
+	}
+	return res
+}
+
+// ReDupWithUint64
+//
+//	@Description: Uint64切片的去重
+//	@receiver a
+//	@param params
+//	@return []uint64
+func (a *ArtifactUtil) ReDupWithUint64(params []uint64) []uint64 {
+	res := make([]uint64, 0, len(params))
+	tmpMap := map[uint64]struct{}{}
 	for _, item := range params {
 		if _, exists := tmpMap[item]; !exists {
 			tmpMap[item] = struct{}{}
